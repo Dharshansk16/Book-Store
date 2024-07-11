6ab2e40c93e98111df744c2fd7aed5cd2cb2fa66
Dharshansk16 <dcodecraft@gmail.com>
2024-07-11 13:17:50 +0530

    feat:Added react-slick slider for featured books section

--- frontend/src/components/FeaturedBookList.jsx
+++ frontend/src/components/FeaturedBookList.jsx
@@ -2,16 +2,53 @@ import React from "react";
 import bookList from "../assets/bookList.json";
 import TextYellow from "../styles/TextYellow";
 import BookCard from "./featured-books-section/BookCard";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 export default function FeaturedBookList() {
   const filteredBooks = bookList.filter((book) => {
     return book.category === "free";
   });
-  console.log(filteredBooks);
+
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <>
-      <div className="mx-w-screen-2xl container mx-auto md:px-10 px-4 flex flex-col justify-start">
-        <div className="flex  flex-col gap-6 mr-4 md:mr-0">
+      <div className="mx-w-screen-2xl container mx-auto md:px-10 px-4 flex flex-col justify-start my-8">
+        <div className="flex  flex-col gap-6  md:mr-0 mb-16">
           <h1 className="text-xl md:text-2xl font-bold">
             Featured <TextYellow text="Books" />
           </h1>
@@ -24,17 +61,19 @@ export default function FeaturedBookList() {
             authors and thought leaders.
           </p>
         </div>
-        <div className="mt-8 flex gap-16 flex-grow flex-wrap">
-          {filteredBooks.map((book, index) => (
-            <BookCard
-              key={index}
-              name={book.name}
-              description={book.description}
-              author={book.author}
-              img={book.img}
-              category={book.category}
-            />
-          ))}
+        <div className="slider-container">
+          <Slider {...settings}>
+            {filteredBooks.map((book, index) => (
+              <BookCard
+                key={index}
+                name={book.name}
+                description={book.description}
+                author={book.author}
+                img={book.img}
+                category={book.category}
+              />
+            ))}
+          </Slider>
         </div>
       </div>
     </>
--- frontend/src/components/featured-books-section/BookCard.jsx
+++ frontend/src/components/featured-books-section/BookCard.jsx
@@ -1,20 +1,32 @@
 import React from "react";
+import Button from "../banner/Button";
 
 export default function BookCard({ name, description, author, category, img }) {
   return (
-    <div>
-      <div className="card bg-base-100 w-96 shadow-xl">
+    <div className="mb-8">
+      <div
+        style={{ height: "500px" }}
+        className="card bg-base-100 w-96 shadow-xl"
+      >
         <figure>
-          <img src={img} alt={author} />
+          <img src={img} alt={author} className="object-cover w-full h-full" />
         </figure>
-        <div className="card-body">
-          <h2 className="card-title">
+        <div className="card-body h-1/3 flex flex-col gap-4">
+          <h2 className="card-title text-md">
             {name}
-            <div className="badge badge-secondary">-{author}</div>
+            <div className="badge badge-secondary">{category}</div>
           </h2>
-          <p>{description}</p>
-          <div className="card-actions justify-end">
-            <div className="badge badge-outline">{category}</div>
+          <div className="flex flex-col justify-center mx-auto w-40">
+            {/* <p className="text-sm">{description}</p> */}
+            <button
+              style={{ borderRadius: "50px" }}
+              className="bg-base-200 px-3 py-2 text-md border border-white   hover:bg-yellow-500  hover:text-white"
+            >
+              Download
+            </button>
+          </div>
+          <div className="card-actions justify-end items-end  ml-4">
+            <div>~{author}</div>
           </div>
         </div>
       </div>
